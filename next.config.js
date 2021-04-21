7710e7c6382aeebcd568dd38331189963a023e17
Ren <nsoharab@gmail.com>
2021-04-21 23:51:46 +0300

    initial publish

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const env = {
+    "APP_LOCALE": "en",
+    "APP_APPLE_TOUCH_ICON": "/favicon128.png",
+    "APP_FAVICON_16x16": "/favicon32.png",
+    "APP_FAVICON_32x32": "/favicon16.png",
+
+    "APP_TITLE": "Is there KFC in Finland?",
+    "APP_DESCRIPTION": "No.",
+    "APP_TYPE": "website",
+    "APP_URL": "https://istherekfc.fi",
+    "APP_IMAGE": "https://istherekfc.fi/og-image.png",
+    "APP_IMAGE_TYPE": "image/png",
+    "APP_IMAGE_WIDTH": "1200",
+    "APP_IMAGE_HEIGHT": "630",
+    "APP_SITE_NAME": "Is there KFC in Finland?",
+    "APP_LOCALE": "en",
+
+    "TWITTER": "https://twitter.com/NowNanoTV"
+}
+
+module.exports = env
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = (props) => {
+  const { Component, pageProps } = props
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta content="ie=edge" httpEquiv="x-ua-compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+        <meta name="og:image" property="og:image" content={process.env.APP_IMAGE} />
+        <meta name="og:image:secure_url" property="og:image:secure_url" content={process.env.APP_IMAGE} />
+
+        <title>{process.env.APP_TITLE}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
